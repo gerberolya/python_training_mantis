2b41570d37b9eb49c3e5887720910b29b36f3cd7
Olga Gerber <gerberolya@gmail.com>
2018-07-20 14:39:54 +0300

    настройка РОР3

--- fixture/application.py
+++ fixture/application.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
 from fixture.james import JamesHelper
-
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 
 class Application:
 
@@ -18,6 +19,8 @@ class Application:
         self.session = SessionHelper(self)
         self.james = JamesHelper(self)
         self.project = ProjectHelper(self)
+        self.mail = MailHelper(self)
+        self.signup = SignupHelper(self)
         self.config = config
         self.base_url = config["web"]["baseUrl"]
 
